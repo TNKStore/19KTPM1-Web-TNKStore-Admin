89697e31f85652f9a40827852effe8cab1544fe7
thienxaso1st <tdht27092001@gmail.com>
2021-11-30 21:35:28 +0700

    First commit

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'TNKStore' });
+});
+
+router.get('/index', function (req, res, next) {
+  res.redirect('/')
+})
+
+router.get('/items-list', function (req, res, next) {
+  res.render('items/items-list',
+      {
+          title: 'TNKStore',
+      });
+});
+
+router.get('/item-editor', function (req, res, next) {
+  res.render('items/item-editor',
+      {
+          title: 'TNKStore',
+      });
+});
+
+module.exports = router;
