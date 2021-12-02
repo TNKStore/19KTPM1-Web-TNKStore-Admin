7acf2091d16664cd54df9b17c72777085f6f6de8
thienxaso1st <tdht27092001@gmail.com>
2021-12-02 10:11:22 +0700

    Update code

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(process.env.DB_CONNECTION_STRING);
+const sequelize = new Sequelize("mysql://thienxaso1st:tdht27092001@db4free.net:3306/tnkdatabase");
 
 module.exports = sequelize;
