c6c38616eb0d148ce66de7b9b2f2863124bdb8ad
thienxaso1st <tdht27092001@gmail.com>
2021-12-09 20:56:14 +0700

    update code

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.use(session({ secret: 'mysecretkey' }));
+app.use(session({ secret: process.env.SESSION_SECRET }));
 app.use(passport.initialize());
 app.use(passport.session());
 
