793c3ab4618533f1aa5785d214f1994a530faa74
thanhnamgl1609 <thanhnamgl1609@github.com>
2021-12-09 20:02:32 +0700

    Add feature to sign in

--- components/auth/authController.js
+++ components/auth/authController.js
@@ -1,4 +1,7 @@
 exports.login = (req, res, next) => {
+    if (req.user) {
+        return res.redirect('/')
+    }
     const wrongPassword = req.query['wrong-password'] !== undefined;
     res.render('auth/login', {
         title: 'Sign in',
