c149b7fd568a30593e61a0f69bcf160c86a4e03b
thanhnamgl1609 <thanhnamgl1609@github.com>
2021-12-09 20:36:06 +0700

    Fix redirect

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 /* GET home page. */
 
 router.get('/', function (req, res, next) {
-    res.redirect('/admin/login');
+    res.redirect('/dashboard');
 })
 
 module.exports = router;
